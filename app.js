60a189775f853ce6287e6fc11b4d9d265263a62f
HALaing <61649323+HALaingDev@users.noreply.github.com>
2020-05-29 23:18:49 +0630

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ GAME RULES:
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 
-*/
+*/window.prompt('Love you YMMH <3');
+
 var score, currentScore, active, dice, dicepic, limit, gamestatus;
 reset();
 limit = window.prompt('Enter Game Winning Points!')
